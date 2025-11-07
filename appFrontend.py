da7068dafe9e47591d9a273f9799803919a107c9
Sranjansahu0 <52071709+Sranjansahu0@users.noreply.github.com>
2025-11-07 12:50:43 +0530

    Update appFrontend.py

--- appFrontend.py
+++ appFrontend.py
@@ -2,8 +2,9 @@ import streamlit as st
 import requests
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-API_URL = "https://data-analyzer-0esa.onrender.com"
+API_URL = "http://127.0.0.1:8000"
 
 st.title("Data Analyzer Web App")
 
@@ -13,6 +14,10 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file)
     st.write("## Preview")
     st.dataframe(df.head())
+
+    # Store correlation matrix so it doesn't disappear on re-run
+    if "corr_matrix" not in st.session_state:
+        st.session_state.corr_matrix = None
 
     column = st.selectbox("Select column to Analyze", df.columns)
 
@@ -35,17 +40,64 @@ if uploaded_file:
         st.json(response.json())
 
     if st.button("Find Correlation"):
-        data_json = df.replace({np.nan: None}).to_dict(orient = "records")
-        response = requests.post(API_URL + "/correlation", json = {"data": data_json})
+            st.write("Correlation Analysis")
+            st.write("### 📊 Data Visualization")
 
-        if response.status_code == 200:
-            result = response.json()
-            if "error" in result:
-                st.error(result["error"])
+            # Convert uploaded data to JSON form for backend
+            data_json = df.replace({np.nan: None}).to_dict(orient="records")  # Convert NaN to None so FastAPI accepts it
+
+            response = requests.post(API_URL + "/correlation", json={"data": data_json})  # Call backend correlation API
+
+            if response.status_code == 200:
+                result = response.json()
+                if "error" in result:
+                    st.error(result["error"])
+                else:
+                    st.session_state.corr_matrix = pd.DataFrame(result["correlation"])
+                    st.success("Correlation matrix generated successfully")
+                    #correlation = result.get("correlation", result)
+                    #corr_df = pd.DataFrame(correlation)
+                    #st.write("### correlation matrix")
+                    #st.dataframe(corr_df, use_container_width = True)'''
             else:
-                correlation = result.get("correlation", result)
-                corr_df = pd.DataFrame(correlation)
-                st.write("### correlation matrix")
-                st.dataframe(corr_df, use_container_width = True)
-        else:
-            st.error("Error fetching correlation matrix")
+                st.error("Error fetching correlation matrix")
+                #corr_df = None
+
+    #---Show visulisation only if correlation exist---
+    if st.session_state.corr_matrix is not None:
+        corr_df = st.session_state.corr_matrix
+
+        st.write("## correlation matrix table")
+        st.dataframe(corr_df, use_container_width = True)
+
+        st.write("---")
+        st.write("## Visualization Options")
+        vis_type = st.selectbox("Choose visualisation Type",["Heatmap","Scatter Plot"])
+
+        # heatmap
+        if vis_type == "Heatmap":
+            st.write("### Heatmap")
+            fig,ax = plt.subplots()
+            cax = ax.matshow(corr_df)
+            fig.colorbar(cax)
+            ax.set_xticks(range(len(corr_df.columns)))
+            ax.set_yticks(range(len(corr_df.columns)))
+            ax.set_xticklabels(corr_df.columns, rotation = 45)
+            ax.set_yticklabels(corr_df.columns)
+            st.pyplot(fig)
+
+        #Scatter plot
+        elif vis_type == "Scatter Plot":
+            st.write("### Scatter Plot between between two numeric columns")
+            numeric_cols = df.select_dtypes(include=['int64','float64']).columns.tolist()
+
+            #Dropdowns for X and Y axes
+            x_col = st.selectbox("Select X-axis", numeric_cols)
+            y_col = st.selectbox("Select Y-axis", numeric_cols)
+
+            fig,ax = plt.subplots()
+            ax.scatter(df[x_col],df[y_col])
+            ax.set_xlabel(x_col)
+            ax.set_ylabel(y_col)
+            ax.set_title(f"{y_col} vs {x_col}")
+            st.pyplot(fig)
