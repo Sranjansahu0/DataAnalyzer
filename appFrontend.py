d8e8a6b42f469b11f9a20d6b34224239ef19b4d3
Sranjansahu0 <52071709+Sranjansahu0@users.noreply.github.com>
2025-11-07 12:56:24 +0530

    Update appFrontend.py

--- appFrontend.py
+++ appFrontend.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://data-analyzer-0esa.onrender.com"
 
 st.title("Data Analyzer Web App")
 
